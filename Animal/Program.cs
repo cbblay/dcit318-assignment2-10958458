07c759f9e245cca9a1a6b7cc3eaafc88e3ba8b79
cbblay <cbblay@st.ug.edu.gh>
2024-06-15 01:03:45 +0000

    valid push

---
+++ Animal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    public class Animal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Some generic Sound");
+        }
+    }
+    class Dog
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Bark!!");
+        }
+    }
+    class Cat
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow!!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal anim = new Animal();
+            anim.MakeSound();
+
+            Dog dog = new Dog();
+            dog.MakeSound();
+
+            Cat cat =new Cat();
+            cat.MakeSound();
+
+        }
+    }
+}
