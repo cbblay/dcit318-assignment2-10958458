dcbfaef5b36cad8c1b823dd5ef5df4549699f39c
cbblay <cbblay@st.ug.edu.gh>
2024-06-15 02:05:43 +0000

    last commit

---
+++ IMovable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMovable
+{
+    internal class Program
+    {
+        public interface IMovable
+        {
+            void Move();
+        }
+        public class Car : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Car is moving.");
+            }
+        }
+        public class Bicycle : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Bicycle is moving.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Move();
+
+            Bicycle bike = new Bicycle();
+            bike.Move();
+        }
+    }
+}
